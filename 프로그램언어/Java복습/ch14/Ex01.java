320c268869f75294fccdfb0112dc014d191b1497
JunHyeong <sure0830@naver.com>
2021-08-19 12:47:01 +0900

    java

---
+++ 프로그램언어/Java복습/ch14/Ex01.java
@@ -0,0 +1,30 @@
+package ch14;
+
+@FunctionalInterface
+interface MyFunction {
+	void run();
+}
+
+public class Ex01 {
+	static void execute(MyFunction f) {
+		f.run();
+	}
+	
+	static MyFunction getMyFunction() {
+		MyFunction f = () -> System.out.println("f3.run()");
+		return f;
+	}
+	
+	public static void main(String[] args) {
+		MyFunction f1 = () -> System.out.println("f1.run()");
+		MyFunction f2 = () -> System.out.println("f2.run()");
+		MyFunction f3 = getMyFunction();
+		
+		f1.run();
+		f2.run();
+		f3.run();
+		
+		execute(f1);
+		execute(() -> System.out.println("f4.run()"));
+	}
+}
