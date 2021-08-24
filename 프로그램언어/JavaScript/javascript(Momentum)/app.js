10b1623d806a04f2a0768b8642aa0cbd6c453509
JunHyeong_An <sure0830@naver.com>
2021-08-24 17:03:21 +0900

    javascript

--- 프로그램언어/JavaScript/javascript(Momentum)/app.js
+++ 프로그램언어/JavaScript/javascript(Momentum)/app.js
@@ -16,3 +16,16 @@ console.log("hello " + myName);
 myName2 = "noloras";
 
 console.log("Your New Name is " + myName2);
+
+///////////////////////////////////////////////////////////
+
+const amIFat = true;
+// on memory, it have space but no value
+// null is never happend automaticly
+// null means empty
+let something;
+console.log(something)
+console.log(amIFat);
+
+const amIFat2 = null;
+console.log(null);
