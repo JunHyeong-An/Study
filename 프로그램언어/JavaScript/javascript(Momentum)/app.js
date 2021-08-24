92ecc88d48415fa3ee6df590ef12b74128cdc2ba
JunHyeong_An <sure0830@naver.com>
2021-08-24 16:40:46 +0900

    javascript

---
+++ 프로그램언어/JavaScript/javascript(Momentum)/app.js
@@ -0,0 +1,10 @@
+const a = 5;
+const b = 2;
+const myName = "nico";
+const veryLongVariableName = 0; // javascript
+const very_long_variable_name = 0; //python
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
