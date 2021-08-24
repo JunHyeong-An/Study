9b97211b672d39b8c31b96f8573ed3a6a81e9c2e
JunHyeong_An <sure0830@naver.com>
2021-08-24 17:18:34 +0900

    javascript

--- 프로그램언어/JavaScript/javascript(Momentum)/app.js
+++ 프로그램언어/JavaScript/javascript(Momentum)/app.js
@@ -29,3 +29,29 @@ console.log(amIFat);
 
 const amIFat2 = null;
 console.log(null);
+
+///////////////////////////////////////////////////////////
+
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const daysOfWeek = [mon, tue, wed, thu, fri, sat, sun];
+const nonsense = [1, 2, "hello", false, null, true, undefined, "name"];
+
+console.log(daysOfWeek);
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
+
+const apple = "apple";
+const potato = "potato";
+
+const toBuy = [apple, potato, "pizza"];
+
+toBuy.push("kimbab");
+
+console.log(toBuy);
